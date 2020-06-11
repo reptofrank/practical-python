32b8ab9f295e93f2dedef08033f56effa9b5c4d9
Obinna Okafor <obinna@fivenines.com.ng>
2020-06-11 12:57:30 +0100

    added extra payment for first 12 months

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -5,9 +5,12 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+count = 0;
 
 while principal > 0:
-    principal = principal * (1+rate/12) - payment
-    total_paid = total_paid + payment
+    extrapayment = payment + 1000 if count < 12 else payment
+    principal = principal * (1+rate/12) - extrapayment
+    total_paid = total_paid + extrapayment
+    count += 1
 
-print('Total paid', total_paid)
+print('Total paid', total_paid, 'Duration', count)
