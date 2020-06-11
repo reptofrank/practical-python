5bf67dc7f21cf21294136b89b6148206931b8807
Obinna Okafor <obinna@fivenines.com.ng>
2020-06-11 14:37:55 +0100

    fixed last month overpayment and used f-strings to format output

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -12,10 +12,16 @@ count = 0;
 
 while principal > 0:
 	count += 1
-	extrapayment = payment + extra_payment if count >= extra_payment_start_month and count <= extra_payment_end_month else payment
-	principal = round(principal * (1+rate/12), 2) - extrapayment
-	total_paid = total_paid + extrapayment
-	print("{} {} {}".format(count, total_paid, principal))
+	# if principal < payment:
+	# 	extrapayment = principal
+	pay = payment + extra_payment if count >= extra_payment_start_month and count < extra_payment_end_month else payment
+	# print("principal - {}".format(principal))
+	principal = round(principal * (1+rate/12), 2)
+	if principal < pay:
+		pay = principal
+	principal = principal - pay
+	total_paid = total_paid + pay
+	print(f"{count} {total_paid} {principal}")
     
 
-print('Total paid', total_paid, 'Duration', count)
+print(f'Total paid: {total_paid}, Duration: {count} months')
