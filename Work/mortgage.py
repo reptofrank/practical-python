5addc843a4d552ddb7ba01cd4bf976981ac825c0
Obinna Okafor <obinna@fivenines.com.ng>
2020-06-11 12:59:45 +0100

    remove hardcoded extra payment info

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -5,12 +5,17 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+extra_payment_start_month = 60
+extra_payment_end_month = 108
+extra_payment = 1000
 count = 0;
 
 while principal > 0:
-    extrapayment = payment + 1000 if count < 12 else payment
-    principal = principal * (1+rate/12) - extrapayment
-    total_paid = total_paid + extrapayment
-    count += 1
+	count += 1
+	extrapayment = payment + extra_payment if count >= extra_payment_start_month and count <= extra_payment_end_month else payment
+	principal = principal * (1+rate/12) - extrapayment
+	total_paid = total_paid + extrapayment
+	print("{} {} {}".format(count, total_paid, principal))
+    
 
 print('Total paid', total_paid, 'Duration', count)
