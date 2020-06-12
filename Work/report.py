b6aa2f606745e75d5d1f69d13c241f09e8015f68
Obinna Okafor <obinna@fivenines.com.ng>
2020-06-12 20:07:43 +0100

    Exercise 2.4: A list of tuples

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,22 @@
 # report.py
 #
 # Exercise 2.4
+import sys
+import csv
+
+def read_portfolio(filename):
+	""" """
+	portfolio = []
+	with open(filename, 'rt') as f:
+		rows = csv.reader(f)
+		headers = next(rows)
+		for row in rows:
+			holding = (row[0], int(row[1]), float(row[2]))
+			portfolio.append(holding)
+
+
+	return portfolio
+
+if __name__ == '__main__':
+	if len(sys.argv) == 2:
+		print(read_portfolio(sys.argv[1]))
