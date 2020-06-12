821f3f8504d3feee9d75076c7f08eeccaa207508
Obinna Okafor <obinna@fivenines.com.ng>
2020-06-12 07:59:20 +0100

    Exercise 1.30: Turning a script into a function

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,11 +1,14 @@
 # pcost.py
 #
 # Exercise 1.27
-with open('Data/portfolio.csv', 'rt') as f:
-	headers = next(f)
-	total = 0
-	for line in f:
-		parts = line.split(',')
-		total += int(parts[1]) * float(parts[2])
+def portfolio_cost(filename):
+	with open(filename, 'rt') as f:
+		headers = next(f)
+		total = 0
+		for line in f:
+			parts = line.split(',')
+			total += int(parts[1]) * float(parts[2])
+		return total
 
-	print(f'Total cost {total}')
+cost = portfolio_cost('Work/Data/portfolio.csv')
+print('Total cost:', cost)
