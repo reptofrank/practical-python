5f33a5bec068aff561f21097f6f94ccb18c5a1ad
Obinna Okafor <obinna@fivenines.com.ng>
2020-06-12 20:52:24 +0100

    Exercise 2.6: Dictionaries as a container

--- Work/report.py
+++ Work/report.py
@@ -5,17 +5,31 @@ import sys
 import csv
 
 def read_portfolio(filename):
-	""" """
+	"""read portfolio information from csv file and return in an array of tuples"""
 	portfolio = []
 	with open(filename, 'rt') as f:
 		rows = csv.reader(f)
 		headers = next(rows)
 		for row in rows:
-			holding = (row[0], int(row[1]), float(row[2]))
+			holding = {'name': row[0], 'shares': int(row[1]), 'price': float(row[2])}
 			portfolio.append(holding)
 
 
 	return portfolio
+
+
+def read_prices(filename):
+	""" """
+	with open(filename, 'rt') as f:
+		rows = csv.reader(f)
+		prices = {}
+		for row in rows:
+			if row:
+				prices[row[0]] = float(row[1])
+
+		return prices
+
+
 
 if __name__ == '__main__':
 	if len(sys.argv) == 2:
